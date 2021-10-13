5d96b9717b300d1b8a77ddf5cd8bbf158a8a2754
Sumeet Agarwal <sumeet.a@gmail.com>
2021-10-13 13:21:19 -0700

    wip

--- compiler01/src/main.rs
+++ compiler01/src/main.rs
@@ -1,12 +1,15 @@
 const EXAMPLE_PROGRAM: &str = r#"
-smt {}
-smt2 {}
+smt {
+    open(300) -> awfp {
+    }
+}
 "#;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     dbg!(EXAMPLE_PROGRAM);
-    let p = parser::program(EXAMPLE_PROGRAM).unwrap();
+    let p = parser::program(EXAMPLE_PROGRAM)?;
     dbg!(p);
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -22,11 +25,26 @@ pub struct Exa {
 
 #[derive(Debug)]
 pub struct Block {
-    exprs: Vec<Expr>,
+    els: Vec<BlockEl>,
 }
 
 #[derive(Debug)]
-pub struct Expr {}
+pub enum BlockEl {
+    OpenFileBlock(OpenFileBlock),
+}
+
+#[derive(Debug)]
+pub struct OpenFileBlock {
+    file_id: NumOrVar,
+    binding: String,
+    block: Block,
+}
+
+#[derive(Debug)]
+pub enum NumOrVar {
+    Var(String),
+    Int(u32),
+}
 
 peg::parser! {
     pub grammar parser() for str {
@@ -37,13 +55,31 @@ peg::parser! {
             = _? name:ident() _? block:block() _? { Exa { name: name.into(), block } }
 
         rule block() -> Block
-            = "{" exprs:(expr_with_newlines()*) "}" { Block { exprs } }
+            = empty_block() / block_with_exprs()
+        rule empty_block() -> Block
+            = "{" _? "}" { Block { els: vec![] }}
+        rule block_with_exprs() -> Block
+            = "{" els:(block_el_with_whitespace()*) "}" { Block { els } }
 
-        rule expr_with_newlines() -> Expr
-            = newline()* expr:expr() newline()* { expr }
+        rule block_el_with_whitespace() -> BlockEl
+            = _* block_el:block_el() _* { block_el }
 
-        rule expr() -> Expr
-            = ident() { Expr {} }
+        rule block_el() -> BlockEl
+            = open_file_block()
+
+        rule open_file_block() -> BlockEl
+            = "open(" _? file_id:num_or_var() _? ")" _? "->" _? binding:ident() _? block:block() {
+                BlockEl::OpenFileBlock(OpenFileBlock { file_id, binding: binding.to_owned(), block })
+            }
+
+        rule num_or_var() -> NumOrVar
+            = num_or_var_num() / num_or_var_var()
+        rule num_or_var_var() -> NumOrVar
+            = ident:ident() { NumOrVar::Var(ident.to_owned()) }
+        rule num_or_var_num() -> NumOrVar
+            = num:num() { NumOrVar::Int(num) }
+        rule num() -> u32
+            = num:$("0" / "-"? ['1' ..= '9']+ ['0' ..= '9']*) {? num.parse().or(Err("num")) }
 
         rule ident() -> &'input str = $(ident_start()+ ['a'..='z' | 'A'..='Z' | '_' | '0'..='9']*)
         rule ident_start() -> &'input str = $(['a'..='z' | 'A'..='Z' | '_']+)
