ff13ea47c927427a7f5c7a8546daaae3c922f78f
Sumeet Agarwal <sumeet.a@gmail.com>
2021-10-13 11:36:00 -0700

    initial

---
+++ compiler01/src/main.rs
@@ -0,0 +1,55 @@
+const EXAMPLE_PROGRAM: &str = r#"
+smt {}
+smt2 {}
+"#;
+
+fn main() {
+    dbg!(EXAMPLE_PROGRAM);
+    let p = parser::program(EXAMPLE_PROGRAM).unwrap();
+    dbg!(p);
+}
+
+#[derive(Debug)]
+pub struct Program {
+    exas: Vec<Exa>,
+}
+
+#[derive(Debug)]
+pub struct Exa {
+    name: String,
+    block: Block,
+}
+
+#[derive(Debug)]
+pub struct Block {
+    exprs: Vec<Expr>,
+}
+
+#[derive(Debug)]
+pub struct Expr {}
+
+peg::parser! {
+    pub grammar parser() for str {
+        pub rule program() -> Program
+            = exas:(exa()+) { Program { exas } }
+
+        rule exa() -> Exa
+            = _? name:ident() _? block:block() _? { Exa { name: name.into(), block } }
+
+        rule block() -> Block
+            // = "{" exprs:(expr()*) "}" { Block(exprs) }
+            = "{}" { Block { exprs: vec![] } }
+
+        // rule expr() -> Expr
+        //     =
+
+        rule ident() -> &'input str = $(ident_start()+ ['a'..='z' | 'A'..='Z' | '_' | '0'..='9']*)
+        rule ident_start() -> &'input str = $(['a'..='z' | 'A'..='Z' | '_']+)
+        rule comma() -> () = _? "," _?
+        rule nbspace() = onespace()+
+        rule onespace() = [' ' | '\t']
+        rule newline() = "\n" / "\r\n"
+        rule whitespace() = (nbspace() / newline())+
+        rule _() = quiet!{ whitespace() };
+    }
+}
