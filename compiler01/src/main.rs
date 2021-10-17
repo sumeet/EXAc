2e840ce70217e2101a66d8ba9622087c647cd679
Sumeet Agarwal <sumeet.a@gmail.com>
2021-10-17 12:20:50 -0700

    almost done adding continue

--- compiler01/src/main.rs
+++ compiler01/src/main.rs
@@ -98,171 +98,202 @@ struct TestExpr {
     negate: bool,
 }
 
-fn compile_condition(cond: &Condition) -> anyhow::Result<TestExpr> {
-    match cond {
-        Condition::Equals(lhs, rhs) => Ok(TestExpr {
-            test_statement: format!("TEST {} = {}", cond_op(lhs)?, cond_op(rhs)?),
-            negate: false,
-        }),
-        Condition::NotEquals(lhs, rhs) => Ok(TestExpr {
-            test_statement: format!("TEST {} = {}", cond_op(lhs)?, cond_op(rhs)?),
-            negate: true,
-        }),
-        Condition::LessThan(lhs, rhs) => Ok(TestExpr {
-            test_statement: format!("TEST {} < {}", cond_op(lhs)?, cond_op(rhs)?),
-            negate: false,
-        }),
-        Condition::GreaterThan(lhs, rhs) => Ok(TestExpr {
-            test_statement: format!("TEST {} > {}", cond_op(lhs)?, cond_op(rhs)?),
-            negate: false,
-        }),
-        Condition::Not(inner_cond) => {
-            let mut inner_test_expr = compile_condition(inner_cond)?;
-            inner_test_expr.negate = !inner_test_expr.negate;
-            Ok(inner_test_expr)
+struct CompileContext {
+    loop_start_labels: Vec<String>,
+}
+
+impl CompileContext {
+    fn new() -> Self {
+        Self {
+            loop_start_labels: vec![],
         }
-        Condition::EOF => Ok(TestExpr {
-            test_statement: "TEST EOF".to_string(),
-            negate: false,
-        }),
-        Condition::ChannelReady => Ok(TestExpr {
-            test_statement: "TEST MRD".to_string(),
-            negate: false,
-        }),
     }
-}
 
-fn compile_while(r#while: &parser::While) -> anyhow::Result<Vec<String>> {
-    let while_id = rand_label_id();
-    let start_label = format!("WH_ST_{}", while_id);
-    let end_label = format!("WH_EN_{}", while_id);
+    fn compile_exa(&mut self, exa: &parser::Exa) -> anyhow::Result<Vec<String>> {
+        self.compile_block(&exa.block)
+    }
 
-    let TestExpr {
-        test_statement,
-        negate: needs_negation,
-    } = compile_condition(&r#while.cond)?;
-    let mut v = vec![format!("MARK {}", start_label), test_statement];
-    let jmp_instruction = if needs_negation { "TJMP" } else { "FJMP" };
-    v.push(format!("{} {}", jmp_instruction, end_label));
-    v.extend(compile_block(&r#while.block)?);
-    v.push(format!("JUMP {}", start_label));
-    v.push(format!("MARK {}", end_label));
-    Ok(v)
-}
+    fn compile_block(&mut self, block: &parser::Block) -> anyhow::Result<Vec<String>> {
+        block
+            .exprs
+            .iter()
+            .map(|expr| match expr {
+                Expr::OpenFileBlock(open_file_block) => {
+                    let mut v = vec![format!("GRAB {}", to_reg_name(&open_file_block.file_id)?)];
+                    v.extend(self.compile_block(&open_file_block.block)?);
+                    v.push("DROP".to_owned());
+                    Ok(v)
+                }
+                Expr::CreateFileBlock(block) => {
+                    let mut v = vec!["MAKE".to_owned()];
+                    v.extend(self.compile_block(block)?);
+                    v.push("DROP".to_owned());
+                    Ok(v)
+                }
+                Expr::Assignment(assignment) => assign_expr(assignment),
+                Expr::ChannelToggle => Ok(vec!["MODE".to_string()]),
+                Expr::ChannelWait => Ok(vec!["VOID M".to_string()]),
+                Expr::Halt => Ok(vec!["HALT".to_string()]),
+                Expr::Kill => Ok(vec!["KILL".to_string()]),
+                Expr::Wait(n) => Ok(repeat("NOOP".to_string()).take(*n as _).collect()),
+                Expr::Link(link) => Ok(link
+                    .dests
+                    .iter()
+                    .map(|dest| format!("LINK {}", to_arg(dest)))
+                    .collect()),
+                Expr::Continue => {
+                    let prev_start_label = self
+                        .loop_start_labels
+                        .last()
+                        .ok_or_else(|| anyhow!("continue used outside of loop"))?;
+                    Ok(vec![format!("JUMP {}", prev_start_label)])
+                }
+                Expr::While(r#while) => self.compile_while(r#while),
+                Expr::Spawn(block) => self.compile_spawn(block),
+                Expr::Loop(block) => self.compile_loop(block),
+                Expr::If(r#if) => self.compile_if(r#if),
+                Expr::FileOp(file_op) => self.compile_file_op(file_op),
+                Expr::LiteralNum(_)
+                | Expr::XVarRef(_)
+                | Expr::TVarRef(_)
+                | Expr::SpecialReg(_)
+                | Expr::GlobalLink(_) => {
+                    bail!("{:?} on bare level not supported", expr)
+                }
+            })
+            .collect::<anyhow::Result<Vec<_>>>()
+            .map(|v| v.into_iter().flatten().collect())
+    }
 
-fn compile_loop(block: &parser::Block) -> anyhow::Result<Vec<String>> {
-    let loop_id = rand_label_id();
-    let start_of_loop_label = format!("LO_ST_{}", loop_id);
+    fn compile_condition(&mut self, cond: &Condition) -> anyhow::Result<TestExpr> {
+        match cond {
+            Condition::Equals(lhs, rhs) => Ok(TestExpr {
+                test_statement: format!("TEST {} = {}", cond_op(lhs)?, cond_op(rhs)?),
+                negate: false,
+            }),
+            Condition::NotEquals(lhs, rhs) => Ok(TestExpr {
+                test_statement: format!("TEST {} = {}", cond_op(lhs)?, cond_op(rhs)?),
+                negate: true,
+            }),
+            Condition::LessThan(lhs, rhs) => Ok(TestExpr {
+                test_statement: format!("TEST {} < {}", cond_op(lhs)?, cond_op(rhs)?),
+                negate: false,
+            }),
+            Condition::GreaterThan(lhs, rhs) => Ok(TestExpr {
+                test_statement: format!("TEST {} > {}", cond_op(lhs)?, cond_op(rhs)?),
+                negate: false,
+            }),
+            Condition::Not(inner_cond) => {
+                let mut inner_test_expr = self.compile_condition(inner_cond)?;
+                inner_test_expr.negate = !inner_test_expr.negate;
+                Ok(inner_test_expr)
+            }
+            Condition::EOF => Ok(TestExpr {
+                test_statement: "TEST EOF".to_string(),
+                negate: false,
+            }),
+            Condition::ChannelReady => Ok(TestExpr {
+                test_statement: "TEST MRD".to_string(),
+                negate: false,
+            }),
+        }
+    }
 
-    let mut v = vec![];
-    v.push(format!("MARK {}", start_of_loop_label));
-    v.extend(compile_block(&block)?);
-    v.push(format!("JUMP {}", start_of_loop_label));
-    Ok(v)
-}
+    fn compile_while(&mut self, r#while: &parser::While) -> anyhow::Result<Vec<String>> {
+        let while_id = rand_label_id();
+        let start_label = format!("WH_ST_{}", while_id);
+        let end_label = format!("WH_EN_{}", while_id);
 
-fn compile_spawn(block: &parser::Block) -> anyhow::Result<Vec<String>> {
-    let spawn_id = rand_label_id();
-    let start_of_spawn_label = format!("SP_ST_{}", spawn_id);
-    let end_of_spawn_label = format!("SP_EN_{}", spawn_id);
-
-    let mut v = vec![];
-    v.push(format!("REPL {}", start_of_spawn_label));
-    v.push(format!("JUMP {}", end_of_spawn_label));
-    v.push(format!("MARK {}", start_of_spawn_label));
-    v.extend(compile_block(&block)?);
-    v.push(format!("MARK {}", end_of_spawn_label));
-    Ok(v)
-}
-
-fn compile_if(r#if: &parser::If) -> anyhow::Result<Vec<String>> {
-    let if_id = rand_label_id();
-    let end_label = format!("IF_EN_{}", if_id);
-    let else_label = format!("IF_EL_{}", if_id);
-
-    let TestExpr {
-        test_statement,
-        negate: needs_negation,
-    } = compile_condition(&r#if.cond)?;
-    let jmp_instruction = if needs_negation { "TJMP" } else { "FJMP" };
-
-    let mut v = vec![test_statement];
-    if r#if.else_block.is_none() {
+        let TestExpr {
+            test_statement,
+            negate: needs_negation,
+        } = self.compile_condition(&r#while.cond)?;
+        let mut v = vec![format!("MARK {}", start_label), test_statement];
+        let jmp_instruction = if needs_negation { "TJMP" } else { "FJMP" };
         v.push(format!("{} {}", jmp_instruction, end_label));
-    } else {
-        v.push(format!("{} {}", jmp_instruction, else_label));
-    }
-    v.extend(compile_block(&r#if.block)?);
-    if let Some(else_block) = &r#if.else_block {
-        v.push(format!("JUMP {}", end_label));
-        v.push(format!("MARK {}", else_label));
-        v.extend(compile_block(else_block)?);
-    }
-    v.push(format!("MARK {}", end_label));
-    Ok(v)
-}
 
-fn compile_file_op(file_op: &FileOp) -> anyhow::Result<Vec<String>> {
-    match file_op.op_name.as_str() {
-        "seek" => {
-            let arg = file_op
-                .arg
-                .as_ref()
-                .ok_or(anyhow!("seek must have an argument"))?;
-            Ok(vec![format!("SEEK {}", to_arg(&arg))])
+        self.push_loop_start_label(start_label.clone());
+        v.extend(self.compile_block(&r#while.block)?);
+        self.pop_loop_start_label();
+
+        v.push(format!("JUMP {}", start_label));
+        v.push(format!("MARK {}", end_label));
+        Ok(v)
+    }
+
+    fn compile_loop(&mut self, block: &parser::Block) -> anyhow::Result<Vec<String>> {
+        let loop_id = rand_label_id();
+        let start_of_loop_label = format!("LO_ST_{}", loop_id);
+
+        let mut v = vec![];
+        v.push(format!("MARK {}", start_of_loop_label));
+        v.extend(self.compile_block(&block)?);
+        v.push(format!("JUMP {}", start_of_loop_label));
+        Ok(v)
+    }
+
+    fn compile_spawn(&mut self, block: &parser::Block) -> anyhow::Result<Vec<String>> {
+        let spawn_id = rand_label_id();
+        let start_of_spawn_label = format!("SP_ST_{}", spawn_id);
+        let end_of_spawn_label = format!("SP_EN_{}", spawn_id);
+
+        let mut v = vec![];
+        v.push(format!("REPL {}", start_of_spawn_label));
+        v.push(format!("JUMP {}", end_of_spawn_label));
+        v.push(format!("MARK {}", start_of_spawn_label));
+        v.extend(self.compile_block(&block)?);
+        v.push(format!("MARK {}", end_of_spawn_label));
+        Ok(v)
+    }
+
+    fn compile_if(&mut self, r#if: &parser::If) -> anyhow::Result<Vec<String>> {
+        let if_id = rand_label_id();
+        let end_label = format!("IF_EN_{}", if_id);
+        let else_label = format!("IF_EL_{}", if_id);
+
+        let TestExpr {
+            test_statement,
+            negate: needs_negation,
+        } = self.compile_condition(&r#if.cond)?;
+        let jmp_instruction = if needs_negation { "TJMP" } else { "FJMP" };
+
+        let mut v = vec![test_statement];
+        if r#if.else_block.is_none() {
+            v.push(format!("{} {}", jmp_instruction, end_label));
+        } else {
+            v.push(format!("{} {}", jmp_instruction, else_label));
         }
-        "wipe" => Ok(vec!["WIPE".to_owned()]),
-        _ => bail!("file operation {:?} unsupported at bare level", file_op),
+        v.extend(self.compile_block(&r#if.block)?);
+        if let Some(else_block) = &r#if.else_block {
+            v.push(format!("JUMP {}", end_label));
+            v.push(format!("MARK {}", else_label));
+            v.extend(self.compile_block(else_block)?);
+        }
+        v.push(format!("MARK {}", end_label));
+        Ok(v)
     }
-}
 
-fn compile_block(block: &parser::Block) -> anyhow::Result<Vec<String>> {
-    block
-        .exprs
-        .iter()
-        .map(|expr| match expr {
-            Expr::OpenFileBlock(open_file_block) => {
-                let mut v = vec![format!("GRAB {}", to_reg_name(&open_file_block.file_id)?)];
-                v.extend(compile_block(&open_file_block.block)?);
-                v.push("DROP".to_owned());
-                Ok(v)
+    fn compile_file_op(&mut self, file_op: &FileOp) -> anyhow::Result<Vec<String>> {
+        match file_op.op_name.as_str() {
+            "seek" => {
+                let arg = file_op
+                    .arg
+                    .as_ref()
+                    .ok_or(anyhow!("seek must have an argument"))?;
+                Ok(vec![format!("SEEK {}", to_arg(&arg))])
             }
-            Expr::CreateFileBlock(block) => {
-                let mut v = vec!["MAKE".to_owned()];
-                v.extend(compile_block(block)?);
-                v.push("DROP".to_owned());
-                Ok(v)
-            }
-            Expr::Assignment(assignment) => assign_expr(assignment),
-            Expr::ChannelToggle => Ok(vec!["MODE".to_string()]),
-            Expr::ChannelWait => Ok(vec!["VOID M".to_string()]),
-            Expr::Halt => Ok(vec!["HALT".to_string()]),
-            Expr::Kill => Ok(vec!["KILL".to_string()]),
-            Expr::Wait(n) => Ok(repeat("NOOP".to_string()).take(*n as _).collect()),
-            Expr::Link(link) => Ok(link
-                .dests
-                .iter()
-                .map(|dest| format!("LINK {}", to_arg(dest)))
-                .collect()),
-            Expr::While(r#while) => compile_while(r#while),
-            Expr::Spawn(block) => compile_spawn(block),
-            Expr::Loop(block) => compile_loop(block),
-            Expr::If(r#if) => compile_if(r#if),
-            Expr::FileOp(file_op) => compile_file_op(file_op),
-            Expr::LiteralNum(_)
-            | Expr::XVarRef(_)
-            | Expr::TVarRef(_)
-            | Expr::SpecialReg(_)
-            | Expr::GlobalLink(_) => {
-                bail!("{:?} on bare level not supported", expr)
-            }
-        })
-        .collect::<anyhow::Result<Vec<_>>>()
-        .map(|v| v.into_iter().flatten().collect())
-}
+            "wipe" => Ok(vec!["WIPE".to_owned()]),
+            _ => bail!("file operation {:?} unsupported at bare level", file_op),
+        }
+    }
 
-fn compile_exa(exa: &parser::Exa) -> anyhow::Result<Vec<String>> {
-    compile_block(&exa.block)
+    fn push_loop_start_label(&mut self, start_label: String) {
+        self.loop_start_labels.push(start_label);
+    }
+
+    fn pop_loop_start_label(&mut self) {
+        self.loop_start_labels.pop();
+    }
 }
 
 fn to_arg(num_or_var: &NumOrVar) -> String {
@@ -292,10 +323,11 @@ fn main() -> anyhow::Result<()> {
         .ok_or(anyhow!("usage: exc <filename>"))?;
     let program_txt = remove_comments(&std::fs::read_to_string(filename)?);
     let program = parser::parser::program(&program_txt)?;
+    let mut compiler = CompileContext::new();
     //dbg!(p);
     for exa in program.exas {
         // println!("-- exa {} --", exa.name);
-        println!("{}", compile_exa(&exa)?.join("\n"));
+        println!("{}", compiler.compile_exa(&exa)?.join("\n"));
         // println!();
         // println!();
     }
